8c79f73c596d09a91f336cba6919ff57a1509afe
DoopieWop <keanu.kling@t-online.de>
2022-04-16 02:21:01 +0200

    Initial commit

---
+++ lua/autorun/client/cl_combinemine.lua
@@ -0,0 +1,18 @@
+language.Add("lua_combine_mine", "Lua Combine Mine")
+
+local mat = Material("sprites/glow04_noz")
+
+hook.Add("PostDrawOpaqueRenderables", "CombineMineSprite", function()
+    for k, v in ipairs(ents.FindByClass("lua_combine_mine")) do
+        if !v:GetNWBool("light") then continue end
+
+        local ang = v:GetAngles()
+        local pos = v:GetPos() + ang:Up() * 10
+
+        local color = v:GetNWVector("lightcolor", Vector(255, 0, 0)):ToColor()
+        color.a = v:GetNWInt("lightalpha", 255)
+
+        render.SetMaterial(mat)
+        render.DrawSprite(pos, 18, 18, color)
+    end
+end)
